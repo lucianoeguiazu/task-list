187471715323cf9d0f9c28cc98b74f9985420932
lucianoeguiazu <eguiazu.luciano@gmail.com>
2023-03-03 23:16:02 -0300

    modificacion 03

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-(() => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
+
     const btn = document.querySelector('[data-form-btn]');
   
     const createTask = (evento) => {
@@ -17,28 +20,13 @@
       titleTask.innerText = value;
       taskContent.appendChild(checkComplete());
       taskContent.appendChild(titleTask);
-  
-      const content = `
-      <i class="fas fa-trash-alt trashIcon icon"></i>`;
+      
       // task.innerHTML = content;
       task.appendChild(taskContent);
+      task.appendChild(deleteIcon());
       list.appendChild(task);
     };
   
     //Arrow functions o funciones anonimas
     btn.addEventListener('click', createTask);
   
-    const checkComplete = () => {
-      const i = document.createElement('i');
-      i.classList.add('far', 'fa-check-square', 'icon');
-      i.addEventListener('click', completeTask);
-      return i;
-    };
-    // Immediately invoked function expression IIFE
-    const completeTask = (event) => {
-      const element = event.target;
-      element.classList.toggle('fas');
-      element.classList.toggle('completeIcon');
-      element.classList.toggle('far');
-    };
-  })();
